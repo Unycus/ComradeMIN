ee474a16d041813ce8f8f55eb5a7130427b17d99
Unycus <unyhou@mail.ru>
2025-09-07 18:08:43 +0300

    тест

--- ComradeMIN/obj/Debug/net8.0-windows/ComradeMIN.AssemblyInfo.cs
+++ ComradeMIN/obj/Debug/net8.0-windows/ComradeMIN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComradeMIN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e6e04c24a0adb63ca7953b3ad6cbf893cf53d66")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca8988ce76398d8d101b0089e2d9897601827032")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComradeMIN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComradeMIN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
