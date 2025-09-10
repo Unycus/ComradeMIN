0cf72f36883b7d315fc623a504d3729ed59cbe66
Chubfull <vebold@mail.ru>
2025-09-10 17:26:13 +0300

    Добавили регистрацию

--- ComradeMIN/RegisterPage.xaml.cs
+++ ComradeMIN/RegisterPage.xaml.cs
@@ -24,5 +24,31 @@ namespace ComradeMIN
         {
             InitializeComponent();
         }
+
+        private void Enter_Click(object sender, RoutedEventArgs e)
+        {
+            NavigationService.Navigate(new LoginPage());
+        }
+
+        private void Enter_to_register_Click(object sender, RoutedEventArgs e)
+        {
+            string username = Login_input.Text;
+            string password = Password_input.Text;
+            bool Succes = true;
+            if (username.Length < 6 || password.Length < 8)
+            {
+                Succes = false;
+                MessageBox.Show("Мало символов, попробуйте ещё раз");
+            }
+            if((Succes)&&Password_input.Text != Password_input2.Text)
+            {
+                Succes = false;
+                MessageBox.Show("Пароли не совпадают");
+            }
+            if (Succes)
+            {
+                MessageBox.Show("Вы успешно зарегистрировались");
+            }
+        }
     }
 }
