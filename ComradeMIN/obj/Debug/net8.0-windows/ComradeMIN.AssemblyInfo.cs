2a67705357547ddd702df5fe52f679133c663514
Chubfull <vebold@mail.ru>
2025-12-03 09:08:33 +0300

    .

--- ComradeMIN/obj/Debug/net8.0-windows/ComradeMIN.AssemblyInfo.cs
+++ ComradeMIN/obj/Debug/net8.0-windows/ComradeMIN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComradeMIN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8f5eaffa8ae432b0c17b0623a924107ec40bc31")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a67d80f72a2469f8ff403f19296d8047205f41fa")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComradeMIN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComradeMIN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
