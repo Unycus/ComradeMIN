16e3fc382e8c6d84b5fcf30c284ed3daa03bc2ce
Unycus <unyhou@mail.ru>
2025-12-19 16:53:58 +0300

    скс4

--- ComradeMIN/obj/Release/net8.0-windows/ComradeMIN.AssemblyInfo.cs
+++ ComradeMIN/obj/Release/net8.0-windows/ComradeMIN.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ComradeMIN")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ee405ec4704e31a02c41c7b0fb20321960c1fceb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc9dceb944625756a7565760f46ae91e6513a5cc")]
 [assembly: System.Reflection.AssemblyProductAttribute("ComradeMIN")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ComradeMIN")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
